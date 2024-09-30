f4782737f2231f49f9e0058516d78aaf9e6a7f4a
quvse <marquesoafonso@gmail.com>
2024-09-30 14:58:25 +0100

    buttons fixed, generators fixed

--- javascript.js
+++ javascript.js
@@ -2,22 +2,19 @@ const grid = document.querySelector(".grid")
 const btnCont = document.querySelector(".btnContainer")
 const gridGen = document.querySelector("#gridGen")
 const newGen = document.querySelector("#newGen")
-let ratio
-
 
 function removeGrid(){
     while (grid.firstChild) {
         grid.removeChild(grid.lastChild);
       }
 }
+
 function createGrid(){
     for(let i=0;i<(16*16);i++){
         let box = document.createElement("div")
         box.setAttribute("class", "box")
         grid.appendChild(box)
-
         const boxed = document.querySelectorAll(".box")
-
         boxed.forEach((box) => {
             box.addEventListener("mouseenter", () => {
                 box.style.backgroundColor = "gray"
@@ -28,42 +25,41 @@ function createGrid(){
         })
     }
 }
+
 function createCustomGrid(input){
-    input = parseInt(prompt("boxes per side? min 16 max 100"))
-    ratio = parseInt(100/input)
-    if (input>100||input<16)
-        input = parseInt(prompt("try again, min 16 max 100"))
-    else
+    input = parseInt(prompt("boxes per side? min 8 max 100"))
+    let ratio = parseFloat(100/input)
+    let boxSize = "height:"+ratio+"%;width:"+ratio+"%; margin: 0px;"
+    if (input>100||input<8){
+        input = parseInt(prompt("Minimum 8, maximum 100"))
+    }else
         for(let i=0;i<(input*input);i++){
             let box = document.createElement("div")
             box.setAttribute("class", "box")
-            box.style.cssText = "height:"+ratio+"%;width:"+ratio+"%;"
+            box.style.cssText = boxSize
             grid.appendChild(box)
-
-
-
-            const boxed = document.querySelectorAll(".box")
-
-            boxed.forEach((box) => {
-                box.addEventListener("mouseenter", () => {
-                    box.style.backgroundColor = "gray"
-                })
-                box.addEventListener("mouseleave", () => {
-                    box.style.backgroundColor = "black"
-                })
-            })
         }
-
-    
+    const boxed = document.querySelectorAll(".box")
+    boxed.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor = "gray"
+        })
+        box.addEventListener("mouseleave", () => {
+            box.style.backgroundColor = "black"
+        })
+    })
 }
+
 function generateCustomGrid(){
     removeGrid()
     createCustomGrid()
 }
+
 function generateDefaultGrid(){
     removeGrid()
     createGrid()
 }
+
 gridGen.addEventListener("click", generateCustomGrid)    
 newGen.addEventListener("click", generateDefaultGrid)
 
